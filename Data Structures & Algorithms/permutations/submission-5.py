fc609c08d9ae97cef5191272cbdce2ed3fb4c2f3
JoeLucas99 <josephlucas9315@gmail.com>
2026-06-14 13:33:34 -0400

    Add: permutations - submission-5

---
+++ Data Structures & Algorithms/permutations/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        perms= [[]]
+        for num in nums:
+            new_perms = []
+            for perm in perms:
+                for i in range(len(perm) + 1):
+                    per = perm.copy()
+                    per.insert(i, num)
+                    new_perms.append(per)
+            perms = new_perms
+        return perms
