877a8f3e4702f1e914c711e92f73ead0dd3d42ce
JoeLucas99 <josephlucas9315@gmail.com>
2026-06-19 12:34:49 -0400

    Add: redundant-connection - submission-6

---
+++ Data Structures & Algorithms/redundant-connection/submission-6.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        # The point is to find the first cycle, not just any
+        # Params: n = len(edges), adj_map HM
+        # Make dfs to check for cycle and return true or false. T = Cycle, F = No Cycle
+        # Go through all VE, append them to list, make new seen bool arr, if dfs True return that n1,n2
+        # If the whole thing passes return empty list
+
+        nodes = len(edges)
+        adj_map = defaultdict(list)
+
+        def dfs(node, prev_node):
+            if seen[node] == True:
+                return True
+            seen[node] = True
+            for neighbor in adj_map[node]:
+                if neighbor == prev_node:
+                    continue
+                if dfs(neighbor, node):
+                    return True
+            return False
+
+        for node1, node2 in edges:
+            adj_map[node1].append(node2)
+            adj_map[node2].append(node1)
+            seen = [False] * (nodes + 1)
+            if dfs(node1, -1) == True:
+                return [node1, node2]
+        return []
